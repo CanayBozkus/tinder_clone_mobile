986bebb47ff20be6d5f1794b24cd47d5f7fad66c
canaybozkus <bozkuscanay@gmail.com>
2021-08-26 12:07:51 +0300

    swipe with dragging animation

--- lib/widgets/main_screen_widgets/swipe_card.dart
+++ lib/widgets/main_screen_widgets/swipe_card.dart
@@ -3,59 +3,98 @@ import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
 enum Movement {
-  right,
-  left,
-  up,
-  none
+  none,
+  drag,
+  click,
+}
+
+enum DragAction {
+  swipe,
+  reset,
+  none,
 }
 
 class SwipeEngine {
 
   late List<Widget> children;
   late AnimationController animationController;
-  void Function(void Function())? stateController;
-  late List<Widget> transitionedChildren;
+  late void Function(void Function()) stateController;
   int frontCardIndex = 0;
   int backCardIndex = 1;
-  Movement movement = Movement.none;
   DragStartDetails? startDetails;
   DragUpdateDetails? updateDetails;
   DragEndDetails? endDetails;
   Animation<Offset>? rangeAnimation;
   Animation<double>? transformAnimation;
-  Alignment alignment = Alignment.center;
   double dragX = 0;
   double dragY = 0;
+  bool dragStarted = false;
+  Movement movement = Movement.none;
+  DragAction dragAction = DragAction.none;
 
   init(){
-    setMovementAnimation(Movement.none);
+    setRangeAnimation(0, 0, 0, 0);
+    setTransformAnimation(0, 0);
+
     animationController.addStatusListener((status) {
       if(status == AnimationStatus.completed){
-        print('complete');
-        //this.frontCardIndex += 1;
-        //this.backCardIndex +=1;
-        this.stateController!((){});
-        //animationController.reset();
-      }
-      if(status == AnimationStatus.forward){
-
+        animationCompleteHandler();
       }
     });
   }
 
+  void animationCompleteHandler(){
+    if(movement == Movement.click){
+      clickAnimationCompleteHandler();
+    }
+
+    else if(movement == Movement.drag){
+      dragAnimationCompleteHandler();
+    }
+
+    movement = Movement.none;
+    setRangeAnimation(0, 0, 0, 0);
+    setTransformAnimation(0, 0);
+    this.stateController((){});
+    animationController.reset();
+  }
+
+  void clickAnimationCompleteHandler(){
+    this.frontCardIndex += 1;
+    this.backCardIndex +=1;
+  }
+
+  void dragAnimationCompleteHandler(){
+    if(dragAction == DragAction.swipe){
+      frontCardIndex+=1;
+      backCardIndex+=1;
+    }
+    dragAction = DragAction.none;
+    setTransformAnimation(0, 0);
+    setRangeAnimation(0, 0, 0, 0);
+    startAnimation();
+  }
+
+
   void reject(){
-    setMovementAnimation(Movement.left);
+    //setMovementAnimation(Movement.left);
+    movement = Movement.click;
+    setRangeAnimation(0, 0, -3, -1);
+    setTransformAnimation(0, -0.2);
     startAnimation();
   }
 
   void like(){
-    setMovementAnimation(Movement.right);
+    //setMovementAnimation(Movement.right);
+    movement = Movement.click;
+    setRangeAnimation(0, 0, 3, -1);
+    setTransformAnimation(0, 0.2);
     startAnimation();
   }
 
   void startAnimation(){
     animationController.forward();
-    stateController!((){});
+    stateController((){});
   }
 
   Widget frontCardBuilder(){
@@ -80,26 +119,23 @@ class SwipeEngine {
     return children[backCardIndex];
   }
 
-  void setMovementAnimation(Movement movement){
-    if(movement == Movement.right){
-      rangeAnimation = Tween<Offset>(begin: Offset.zero, end: const Offset(3, -1),).animate(animationController);
-      transformAnimation = Tween<double>(begin:0, end: 0.2).animate(animationController);
-    }
+  void setRangeAnimation(double startX, double startY, double endX, double endY){
+    rangeAnimation = Tween<Offset>(
+      begin: Offset(startX, startY),
+      end: Offset(endX, endY),
+    ).animate(animationController);
+  }
 
-    else if (movement == Movement.left){
-      rangeAnimation = Tween<Offset>(begin: Offset.zero, end: Offset(-3, -1),).animate(animationController);
-      transformAnimation = Tween<double>(begin:0, end: -0.2).animate(animationController);
-    }
-
-    else {
-      rangeAnimation = Tween<Offset>(begin: Offset.zero, end: Offset(0, 0),).animate(animationController);
-      transformAnimation = Tween<double>(begin:0, end: 0).animate(animationController);
-    }
+  void setTransformAnimation(double begin, double end){
+    transformAnimation = Tween<double>(
+      begin: begin,
+      end: end,
+    ).animate(animationController);
   }
 
   void dragStart(DragStartDetails details){
     startDetails = details;
-
+    movement = Movement.drag;
   }
 
   void dragUpdate(DragUpdateDetails details, Size size){
@@ -109,7 +145,7 @@ class SwipeEngine {
     }
     double x = dragX + (details.delta.dx)/(size.width *2);
     double y = dragY + (details.delta.dy)/(size.height *2);
-    print('x: $x    y:$y');
+
     rangeAnimation = Tween<Offset>(
       begin: Offset(dragX, dragY),
       end: Offset(x, y),
@@ -124,6 +160,34 @@ class SwipeEngine {
 
   void dragEnd(DragEndDetails details){
     endDetails = details;
+    print('end');
+
+    if(dragX > -0.3 && dragX < 0.3){
+      dragAction = DragAction.reset;
+      setRangeAnimation(dragX, dragY, 0, 0);
+      setTransformAnimation(dragX / 10, 0);
+      startAnimation();
+      dragX = 0;
+      dragY = 0;
+    }
+
+    else if(dragX < -0.3){
+      dragAction = DragAction.swipe;
+      setRangeAnimation(dragX, dragY, -3, -1);
+      setTransformAnimation(dragX / 10, -0.2);
+      startAnimation();
+      dragX = 0;
+      dragY = 0;
+    }
+
+    else if(dragX > -0.3){
+      dragAction = DragAction.swipe;
+      setRangeAnimation(dragX, dragY, 3, -1);
+      setTransformAnimation(dragX / 10, 0.2);
+      startAnimation();
+      dragX = 0;
+      dragY = 0;
+    }
 
   }
 }
@@ -149,11 +213,9 @@ class _SwipeCardState extends State<SwipeCard> with SingleTickerProviderStateMix
    widget.engine.stateController = setState;
    widget.engine.children = widget.children.reversed.toList();
 
-   controller = AnimationController(duration: const Duration(seconds: 10), vsync: this);
+   controller = AnimationController(duration: const Duration(seconds: 1), vsync: this);
    widget.engine.animationController = controller;
    widget.engine.init();
-
-
 
   }
 
@@ -167,7 +229,7 @@ class _SwipeCardState extends State<SwipeCard> with SingleTickerProviderStateMix
 
     return Stack(
       children: [
-        //widget.engine.backCardBuilder(),
+        widget.engine.backCardBuilder(),
         GestureDetector(
           onPanStart: (DragStartDetails details){
             widget.engine.dragStart(details);
